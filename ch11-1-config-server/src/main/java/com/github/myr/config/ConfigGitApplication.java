34dc51d4b558144ec6e83902caaa4b24ab9d4ba6
myr666233 <598151844@qq.com>
2020-03-31 16:30:38 +0800

    ch11练习

---
+++ ch11-1-config-server/src/main/java/com/github/myr/config/ConfigGitApplication.java
@@ -0,0 +1,17 @@
+package com.github.myr.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author moyiran
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigGitApplication.class, args);
+    }
+}
